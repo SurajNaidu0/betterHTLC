7f4cc35afc2435b989aff851e13910e28742a078
SurajNaidu0 <surajangurajnaidu@gmail.com>
2025-04-28 14:00:02 +0530

    added refund with fee

--- src/htlc/scripts.rs
+++ src/htlc/scripts.rs
@@ -248,7 +248,89 @@ pub(crate) fn htlc_redeem_script_with_fee(reedeem_address:&Address,payment_hash:
     // println!("script: {:?}", script);
     script
 }
-// pub(crate) fn htlc_instant_refund(x_only_pubkey: XOnlyPublicKey) {
-   
-// }
+
+pub(crate) fn htlc_refund_script_with_fee(refund_address:&Address, lock_time: &i64) -> ScriptBuf {
+    let mut builder = Script::builder();
+    builder = builder
+    .push_int(*lock_time)
+    .push_opcode(OP_CSV) 
+    .push_opcode(OP_DROP)
+    .push_opcode(OP_TOALTSTACK)
+    .push_opcode(OP_TOALTSTACK)
+    .push_opcode(OP_TOALTSTACK) 
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_SWAP)
+    // Push [length, script_pubkey_bytes...]
+        .push_slice({
+            let script_pubkey = refund_address.script_pubkey(); // Store Script
+            let script_bytes = script_pubkey.as_bytes(); // Borrow
+            let length = script_bytes.len() as u8;
+            if script_bytes.len() > 255 {
+                panic!("ScriptPubKey too long: {} bytes", script_bytes.len());
+            }
+            let mut bytes_with_length = vec![length];
+            bytes_with_length.extend_from_slice(script_bytes);
+            PushBytesBuf::try_from(bytes_with_length).expect("Invalid scriptPubKey bytes")
+        })
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_SHA256)
+    .push_opcode(OP_SWAP)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_opcode(OP_CAT)
+    .push_slice(*TAPSIGHASH_TAG) // push tag
+        .push_opcode(OP_SHA256) // hash tag
+        .push_opcode(OP_DUP) // dup hash
+        .push_opcode(OP_ROT) // move the sighash to the top of the stack
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_SHA256) 
+        .push_slice(*BIP0340_CHALLENGE_TAG) 
+        .push_opcode(OP_SHA256)
+        .push_opcode(OP_DUP)
+        .push_opcode(OP_ROT) // bring challenge to the top of the stack
+        .push_slice(*G_X) // G is used for the pubkey and K
+        .push_opcode(OP_DUP)
+        .push_opcode(OP_DUP)
+        .push_opcode(OP_DUP)
+        .push_opcode(OP_TOALTSTACK) 
+        .push_opcode(OP_TOALTSTACK) 
+        .push_opcode(OP_ROT) 
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_CAT) 
+        .push_opcode(OP_SHA256) 
+        .push_opcode(OP_FROMALTSTACK) 
+        .push_opcode(OP_SWAP)
+        .push_opcode(OP_CAT) 
+        .push_opcode(OP_FROMALTSTACK) 
+        .push_opcode(OP_FROMALTSTACK) 
+        .push_opcode(OP_ROT)
+        .push_opcode(OP_SWAP) 
+        .push_opcode(OP_DUP) 
+        .push_opcode(OP_FROMALTSTACK) 
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_ROT) 
+        .push_opcode(OP_EQUALVERIFY) 
+        .push_opcode(OP_FROMALTSTACK) 
+        .push_opcode(OP_CAT)
+        .push_opcode(OP_SWAP) 
+        .push_opcode(OP_CHECKSIG);
+    let script = builder.into_script();
+  
+  // println!("script: {:?}", script);
+  script
+}
+
+
 
